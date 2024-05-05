5d5226bb9914c693e5b329834071d18802bb5759
TohidAbdullazade <tohidabdullazade@gmail.com>
2024-05-05 22:53:18 +0400

    I write the task but uncompleted

--- src/script/app.js
+++ src/script/app.js
@@ -1,54 +1,39 @@
-let slides = document.querySelectorAll(".slide");
-let btns = document.querySelectorAll(".btn");
-let currSlide = 0;
-let autoPlayInterval = null;
+// ===> Run All Methods <===
+document.addEventListener("DOMContentLoaded", runAllMethods);
+function runAllMethods(e) {
+    if (e.target) {
+        asideObject.showSideBar();
+        asideObject.hideSideBar();
+    }
+};
 
-// ===> Manual SlideShow Function <===
-const slideShow = (manual) => {
-    slides.forEach((sld) => {
-        sld.classList.remove("active");
-        btns.forEach((btn) => {
-            btn.classList.remove("active")
+// ===> Sideabar Object <===
+let asideObject = {
+    aside: document.getElementById("aside"),
+    openBtn: document.getElementById("openBtn"),
+    closeBtn: document.getElementById("closeBtn"),
+
+
+    // ===> Show Sidebar Menu <===
+    showSideBar: function () {
+        this.openBtn.addEventListener("click", (e) => {
+            if (e.target) {
+                this.aside.style.transform = `translate(21.25rem)`
+                this.closeBtn.style.display = 'block'
+                document.body.style.background = `rgba(0, 0, 0, 0.5)`
+
+            };
+        });
+    },
+    // ===> Hide Sidebar Menu <===
+    hideSideBar: function () {
+        this.closeBtn.addEventListener("click", (e) => {
+            if (e.target) {
+                this.aside.style.transform = `translate(41.875rem)`
+                document.body.style.background = ""
+            }
         })
-    });
-
-    slides[manual].classList.add("active");
-    btns[manual].classList.add("active");
-}
-btns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-        slideShow(i);
-        currSlide = i;
-    })
-})
+    }
+};
 
 
-
-// ===> Auto Play SlideShow Function <===
-const startAutoPlay = () => {
-    autoPlayInterval = setInterval(() => {
-        currSlide++;
-        if (currSlide >= 5) {
-            currSlide = 0;
-        }
-        slideShow(currSlide);
-    }, 3500);
-}
-
-const stopAutoPlay = () => {
-    clearInterval(autoPlayInterval);
-}
-
-startAutoPlay();
-
-btns.forEach((btn) => {
-    btn.addEventListener("mouseenter", () => {
-        stopAutoPlay();
-    });
-});
-
-btns.forEach((btn) => {
-    btn.addEventListener("mouseout", () => {
-        startAutoPlay();
-    });
-});
